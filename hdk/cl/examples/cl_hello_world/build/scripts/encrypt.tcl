74f9b3b7d7dcce0cc422e9306a60a739beffff3b
Moonquakes <467946553@qq.com>
2022-06-01 10:22:19 +0800

    Initialize the frame of wujian soc

--- hdk/cl/examples/cl_hello_world/build/scripts/encrypt.tcl
+++ hdk/cl/examples/cl_hello_world/build/scripts/encrypt.tcl
@@ -34,7 +34,12 @@ if {[llength [glob -nocomplain -dir $TARGET_DIR *]] != 0} {
 file copy -force $CL_DIR/design/cl_hello_world_defines.vh             $TARGET_DIR
 file copy -force $CL_DIR/design/cl_id_defines.vh                      $TARGET_DIR
 file copy -force $CL_DIR/design/cl_hello_world.sv                     $TARGET_DIR 
-file copy -force $CL_DIR/../common/design/cl_common_defines.vh        $TARGET_DIR 
+file copy -force $CL_DIR/../common/design/cl_common_defines.vh        $TARGET_DIR
+
+file copy -force $CL_DIR/design/wujian_soc/params/*.vh                $TARGET_DIR
+file copy -force $CL_DIR/design/wujian_soc/sim_lib/*.v                $TARGET_DIR
+file copy -force $CL_DIR/design/wujian_soc/*.v                        $TARGET_DIR
+
 file copy -force $UNUSED_TEMPLATES_DIR/unused_apppf_irq_template.inc  $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_cl_sda_template.inc     $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_ddr_a_b_d_template.inc  $TARGET_DIR
