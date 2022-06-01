2557c303881f68641fea9f1737d1195fc2a7c243
Moonquakes <467946553@qq.com>
2022-06-01 20:17:13 +0800

    update test

--- hdk/cl/examples/cl_hello_world/verif/scripts/top.vcs.f
+++ hdk/cl/examples/cl_hello_world/verif/scripts/top.vcs.f
@@ -64,7 +64,7 @@ ${HDK_SHELL_DESIGN_DIR}/ip/axi_clock_converter_0/hdl/axi_clock_converter_v2_1_vl
 ${SH_LIB_DIR}/../ip/axi_clock_converter_0/sim/axi_clock_converter_0.v
 
 ${CL_ROOT}/design/cl_hello_world.sv
-${CL_ROOT}/design/wujian_soc/test.v
+${CL_ROOT}/design/test.v
 
 -f ${HDK_COMMON_DIR}/verif/tb/filelists/tb.${SIMULATOR}.f
 
